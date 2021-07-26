c566ab232eb5f0be293cdb10907670a20f850986
sydneyholubow <sydneyhholubow@gmail.com>
2021-07-26 17:43:45 +0000

    firebase files

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,5 +1,14 @@
 const signInButton = document.querySelector(".button")
 signInButton.addEventListener("click", () => {
     console.log("Clicked!")
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user
+        
+        console.log("Login Success", user)
+    })
+    .catch(error => {
+        console.log("login failed", error)
+    })
 })
     
